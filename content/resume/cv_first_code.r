8c9b61dddd8c00e3c5c0bead322325b1088146be
Pedro Neto <pedroceciliocn@gmail.com>
2021-05-18 21:16:50 -0300

    acho que vai dar merda

---
+++ content/resume/cv_first_code.r
@@ -0,0 +1,9 @@
+datadrivencv::use_datadriven_cv(
+  full_name = "Pedro Neto",
+  data_location = "https://docs.google.com/spreadsheets/d/1EOKk2wtW4W_Zp3PGI7nW4KZoeUxpFq1PQvzo0B64xp8/edit#gid=917338460",
+  pdf_location = "https://github.com/pedroceciliocn/cv-online/index.pdf",
+  html_location = "https://pedroccneto.netlify.app/media/index.html",
+  source_location = "https://github.com/pedroceciliocn/pedroissoai",
+  use_network_logo = TRUE,
+  open_files = TRUE
+)
